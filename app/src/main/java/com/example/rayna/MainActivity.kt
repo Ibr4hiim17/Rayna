8d07dcad40cc74065a37ce9fd43749f6719b6d9f
Damim_X_Adi <imadeddinekir95@gmail.com>
2025-03-20 00:10:20 +0100

    Bahi Layes TP02

--- app/src/main/java/com/example/rayna/MainActivity.kt
+++ app/src/main/java/com/example/rayna/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -18,7 +17,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.rayna.R
-import com.example.reviewapp.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
